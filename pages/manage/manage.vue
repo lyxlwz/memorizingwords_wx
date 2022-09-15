360ce4786a6c426e789eec16c5734bc717124c10
Y1mo <94829750+Y1mo@users.noreply.github.com>
2022-09-15 18:36:17 +0800

    manage，result

--- pages/manage/manage.vue
+++ pages/manage/manage.vue
@@ -1,101 +1,142 @@
 <template>
-  <view class="wordPageSty">
-    <!-- 公共组件-每个页面必须引入 -->
-    <public-module></public-module>
-    <view
-      class="bg"
-      style="height: 90rpx;background: #3d5cff;"
-    >
-      <view
-        class="text"
-        style="color: white;font-weight: 200%;padding-top: 20rpx;"
-      >
-        单词管理
-      </view>
+	<view class="wordPageSty">
+		<!-- 公共组件-每个页面必须引入 -->
+		<public-module></public-module>
+		<view class="bg" style="height: 90rpx;background: #3d5cff;">
+			<view class="text" style="color: white;font-weight: 200%;padding-top: 20rpx;">
+				单词管理
+			</view>
 
-    </view>
-    <view class="search">
-      <u-search
-        shape="square"
-        placeholder="今日单词学习"
-        placeholderColor="#3d5cff"
-        bgColor="#ebdffd"
-        height="90rpx"
-        :showAction="false"
-        :clearabled="true"
-      ></u-search>
-    </view>
+		</view>
+		<view class="search">
+			<u--input shape="square" placeholder="今日单词学习" clearable placeholderStyle="color:#3d5cff" bgColor="#ebdffd"
+				height="90rpx" prefixIcon="search" prefixIconStyle="font-size: 22px;color: #909399">
 
-    <!-- // 列表-->
-    <view class="u-page padding-top-lg">
-      <u-list @scrolltolower="scrolltolower">
-        <u-list-item
-          v-for="(item, index) in indexList"
-          :key="index"
-          @click="click(index)"
-        >
-          <u-cell
-            :title="`2022-9-6     on`"
-            value="内容"
-          >
 
-          </u-cell>
-        </u-list-item>
-      </u-list>
-    </view>
-    <z-navigation></z-navigation>
-  </view>
+				<template slot="suffix">
+					<u-popup :show="show" mode="bottom" :round="10" :closeable="true" closeIconPos="top-left"
+						:closeOnClickOverlay="false" @open="open" @close="close">
+						<view class="sys" style="height: 1000rpx;width: 90%;">
+							<view style="padding: 40rpx 300rpx;">设置</view>
+							<view style="font-size: 120%;font-weight: 500;padding-left: 20rpx;">搜索条件</view>
+							<view class="u-page__tag-item" v-for="(item, index) in radios" :key="index">
+								<u-tag :text="item.name" size="large" :plain="!item.checked" type="warning" :name="index" :bgColor="bgColor" :borderColor="borderColor" :color="color"
+									@click="radioClick">
+								</u-tag>
+							</view>
+						</view>
+					</u-popup>
+					<u-button @click="show = true">打开</u-button>
+				</template>
+
+			</u--input>
+		</view>
+
+		<!-- // 列表-->
+		<view class="u-page padding-top-lg">
+			<u-list @scrolltolower="scrolltolower">
+				<u-list-item v-for="(item, index) in indexList" :key="index" @click="click(index)">
+					<u-cell :title="`2022-9-6     on`" value="内容">
+
+					</u-cell>
+				</u-list-item>
+			</u-list>
+		</view>
+		<z-navigation></z-navigation>
+	</view>
 
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      indexList: [],
-      urls: [
+	export default {
+		data() {
+			return {
+				indexList: [],
+				bgColor:"#3d5cff",
+				borderColor:"#3d5cff",
+				color:"#FFFF",
+				urls: [],
+				show: false,
+				radios: [{
+						checked: false,
+						name:"单词"
+					},
+					{
+						checked: false,
+						name:"释义"
+					},
+					{
+						checked: false,
+						name:"学习日期"
+					}
+				],
+				
+			}
+		},
+		//第一次加载
+		onLoad(e) {
+			this.loadmore()
+			// 隐藏原生的tabbar
+			uni.hideTabBar();
+		},
+		//页面显示
+		onShow() {
+			// 隐藏原生的tabbar
+			uni.hideTabBar();
+		},
+		methods: {
+			radioClick(name) {
+				this.radios.map((item, index) => {
+					// item.checked = index === name ? true : false
+					item.checked = index === name
+					if(item.checked=true){
+						this.bgColor="#d7d7d7",
+						this.borderColor="#d7d7d7",
+						this.color="#a6a6a6"
+					}
+				})
+				
+			},
+			click(index) {
+				console.log(index)
+			},
+			scrolltolower() {
+				this.loadmore()
+			},
+			loadmore() {
+				for (let i = 0; i <= 15; i++) {
+					this.indexList.push({
+						url: this.urls[uni.$u.random(0, 1)]
+					})
+				}
+			},
+			open() {
+				// console.log('open');
+			},
+			close() {
+				this.show = false
+				// console.log('close');
+			}
 
-      ]
-    }
-  },
-  //第一次加载
-  onLoad(e) {
-    this.loadmore()
-    // 隐藏原生的tabbar
-    uni.hideTabBar();
-  },
-  //页面显示
-  onShow() {
-    // 隐藏原生的tabbar
-    uni.hideTabBar();
-  },
-  methods: {
-    click(index) {
-      console.log(index)
-    },
-    scrolltolower() {
-      this.loadmore()
-    },
-    loadmore() {
-      for (let i = 0; i <= 15; i++) {
-        this.indexList.push({
-          url: this.urls[uni.$u.random(0, 1)]
-        })
-      }
-    }
-  }
+		}
 
-}
+	}
 </script>
 <style lang="scss" scoped>
-.search {
-  margin: auto;
-  padding-top: 20rpx;
-  width: 90%;
-  height: 42px;
-}
-.u-page {
-  height: calc(100vh - 100rpx);
-  overflow: scroll;
-}
+	.search {
+		margin: auto;
+		padding-top: 20rpx;
+		width: 90%;
+		height: 42px;
+		.u-page__tag-item {
+			width: 230rpx;
+				margin-right: 20px;
+				padding: 20rpx;
+			}
+	}
+
+	.u-page {
+		height: calc(100vh - 100rpx);
+		overflow: scroll;
+	}
 </style>
---
+++ pages/word/results.vue
@@ -0,0 +1,56 @@
+<template>
+	<view class="result">
+		<view>
+			<u-navbar
+			bgColor="#3d5cff"
+			leftIconColor="#cbcdce"
+			            @rightClick="rightClick"
+			            :autoBack="true"
+			        >
+			        </u-navbar>
+		</view>
+		<view class="all">
+			<view class="title">resort</view>
+			<u-button class="custom-style" type="primary" icon="map" size="mini" text="英" ></u-button>
+		</view>
+		
+		<!-- 公共组件-每个页面必须引入 -->
+		<public-module></public-module>
+		<z-navigation></z-navigation>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+<style lang="scss" scoped>
+.result{
+	height: 100vh;
+	background: #3d5cff;
+	.all{
+		margin: auto;
+		width: 90%;
+		.title{
+		padding-top: 150rpx;
+		color: white;
+		padding-left: 20rpx;
+		font-size: 100rpx;
+		font-weight: 200%;
+		.custom-style{
+			// margin: 0 200rpx;
+			width: 50rpx;
+		}
+	}
+	}
+	
+}
+</style>
