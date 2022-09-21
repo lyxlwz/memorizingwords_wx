57118d913e4a5bd6ab91dbfb6ee8b67146e4be37
Y1mo <94829750+Y1mo@users.noreply.github.com>
2022-09-21 15:53:18 +0800

    userr

---
+++ pages/number/number.vue
@@ -0,0 +1,72 @@
+<template>
+	<view class="number">
+		<view>
+		  <u-navbar
+		    bgColor="#3d5cff"
+		    leftIconColor="#cbcdce"
+		    @rightClick="rightClick"
+		    :autoBack="true"
+		  >
+		  </u-navbar>
+		</view>
+		<view class="fbl " style="padding: 300rpx 120rpx; ">
+			<view class="text flex_x_center word-text-border"  >
+				数字背诵训练
+			</view>
+			<view class="input flex_x_center" style="padding-top: 150rpx; ">
+				<view class=" word-Border-radius" style="width: 60%;height: 100rpx; background: #516dff; padding-top:20rpx">
+					<u--input
+				    border="none"
+					fontSize="40rpx"
+					inputAlign="center"
+					color="#CBCDCE"
+				  ></u--input>
+				</view>
+			</view>
+			<view class=" flex_x_center" style="padding-top: 80rpx  ;">
+				<!--iconColor="#627bff" <button type="primary" iconColor="#3d5cff"  style="width: 55%;" @click="yes"  shape="circle">开始筛查</button> -->
+				<view
+				  class="word-text-border but"
+				  style="font-size: 30rpx; padding: 10rpx 40rpx; border-radius: 50upx;"
+				 @click="yes">
+				  开始训练
+				</view>
+			</view>
+		</view>
+		<!-- 公共组件-每个页面必须引入 -->
+		<public-module></public-module>
+		<z-navigation></z-navigation>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			yes(){
+				console.log("kaishi")
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.number{
+		background: #3d5cff;
+		height: 100vh;
+		.text{
+			font-size: 40rpx; 
+			font-weight: 1000;
+		}
+		.but{
+			background: #627bff;
+			width: 200rpx;
+			// height: 20%;
+		}
+	}
+</style>
+
